34a8819502808c06db7e93ef475424bc792707b5
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2022-05-06 23:23:15 -0400

    Update test.rkt

--- test.rkt
+++ test.rkt
@@ -2,85 +2,90 @@
 (require "parse.rkt")
 (require "show.rkt")
 (require "interp.rkt")
-(require rackunit)
 
-(define (run e)
-  (show (eval (parse e))))
+;; smol/fun
 
-(test-equal? "arithmetic operators"
-             (run '((deffun (f o) (o 1 1))
-                    (f +)))
-             '(Error))
-(test-equal? "0 as condition"
-             (run '((if 0 #t #f)))
-             '(#t))
-(test-equal? "redeclare var using defvar"
-             (run '((defvar x 0)
-                    (defvar y x)
-                    (defvar x 2)
-                    x
-                    y))
-             '(Error))
-(test-equal? "expose local defvar"
-             (run '((defvar x 42)
-                    (deffun (create)
-                      (defvar y 42)
-                      y)
+(module+ test
+  (require rackunit)
 
-                    (create)
-                    (equal? x y)))
-             '(Error))
-(test-equal? "pair?"
-             (run '((pair? (pair 1 2))
-                    (pair? (ivec 1 2))
-                    (pair? '#(1 2))
-                    (pair? '(1 2))))
-             '(#t #t #t #f))
-(test-equal? "let* and let"
-             (run '((let* ([v 1]
-                           [w (+ v 2)]
-                           [y (* w w)])
-                      (let ([v 3]
-                            [y (* v w)])
-                        y))))
-             '(3))
+  (define (run e)
+    (show (eval (parse e))))
 
-(test-equal? "defvar and let"
-             (run '((defvar x 3)
-                    (defvar y (let ([y 6] [x 5]) x))
-                    (* x y)))
-             '(15))
-(test-equal? "fun-id equals to arg-id"
-             (run '((deffun (f f) f)
-                    (f 5)))
-             '(5))
-(test-equal? "scoping rule of let"
-             (run '((let ([x 4]
-                          [y (+ x 10)])
-                      y)))
-             '(Error))
-(test-equal? "the right component of ivec"
-             (run '(right (ivec 1 2 3)))
-             '(Error))
-(test-equal? "identifiers"
-             (run '((defvar x 5)
+  (test-equal? "arithmetic operators"
+               (run '((deffun (f o) (o 1 1))
+                      (f +)))
+               '(Error))
+  (test-equal? "0 as condition"
+               (run '((if 0 #t #f)))
+               '(#t))
+  (test-equal? "redeclare var using defvar"
+               (run '((defvar x 0)
+                      (defvar y x)
+                      (defvar x 2)
+                      x
+                      y))
+               '(Error))
+  (test-equal? "expose local defvar"
+               (run '((defvar x 42)
+                      (deffun (create)
+                        (defvar y 42)
+                        y)
 
-                    (deffun (reassign var_name new_val)
-                      (defvar var_name new_val)
-                      (pair var_name x))
+                      (create)
+                      (equal? x y)))
+               '(Error))
+  (test-equal? "pair?"
+               (run '((pair? (pair 1 2))
+                      (pair? (ivec 1 2))
+                      (pair? '#(1 2))
+                      (pair? '(1 2))))
+               '(#t #t #t #f))
+  (test-equal? "let* and let"
+               (run '((let* ([v 1]
+                             [w (+ v 2)]
+                             [y (* w w)])
+                        (let ([v 3]
+                              [y (* v w)])
+                          y))))
+               '(3))
 
-                    (reassign x 6)
-                    x))
-             '('#(6 5) 5))
-(test-equal? "defvar, deffun, and let"
-             (run '((defvar a 1)
-                    (deffun (what-is-a) a)
-                    (let ([a 2])
-                      (ivec
-                       (what-is-a)
-                       a))))
-             '('#(1 2)))
-(test-equal? "syntax pitfall"
-             (run '((deffun (f a b) a + b)
-                    (f 5 10)))
-             '(10))
+  (test-equal? "defvar and let"
+               (run '((defvar x 3)
+                      (defvar y (let ([y 6] [x 5]) x))
+                      (* x y)))
+               '(15))
+  (test-equal? "fun-id equals to arg-id"
+               (run '((deffun (f f) f)
+                      (f 5)))
+               '(5))
+  (test-equal? "scoping rule of let"
+               (run '((let ([x 4]
+                            [y (+ x 10)])
+                        y)))
+               '(Error))
+  (test-equal? "the right component of ivec"
+               (run '(right (ivec 1 2 3)))
+               '(Error))
+  (test-equal? "identifiers"
+               (run '((defvar x 5)
+
+                      (deffun (reassign var_name new_val)
+                        (defvar var_name new_val)
+                        (pair var_name x))
+
+                      (reassign x 6)
+                      x))
+               '('#(6 5) 5))
+  (test-equal? "defvar, deffun, and let"
+               (run '((defvar a 1)
+                      (deffun (what-is-a) a)
+                      (let ([a 2])
+                        (ivec
+                         (what-is-a)
+                         a))))
+               '('#(1 2)))
+  (test-equal? "syntax pitfall"
+               (run '((deffun (f a b) a + b)
+                      (f 5 10)))
+               '(10))
+  )
