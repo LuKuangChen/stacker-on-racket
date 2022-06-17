da6b38325320bca8c89cc12123d2a6c33211b16d
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2022-06-17 16:53:10 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -2,7 +2,6 @@
 (define collection "cs19-step")
 (define deps '("base" "plait" "pict-lib" "pprint" "gui-lib" "draw-lib" "redex-gui-lib" "sandbox-lib" "testing-util-lib" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-#;
 (define scribblings '(("scribblings/cs19-step.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.1")
